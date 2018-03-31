950513a295d7f72d50f00c6f0c13834a761e6b58
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-03-31 11:19:04 -0400

    Assignment 16 completed2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'random_data'
 
+Post.destroy_all
+Comment.destroy_all
 # Create Posts
 50.times do
   Post.create!(
@@ -9,13 +11,23 @@ require 'random_data'
 end
 posts = Post.all
 
- # Create Comments
- 100.times do
+# Create Comments
+100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+puts "#{Post.count}."
+Post.find_or_create_by!(title: "Title", body: "Body for my post")
+puts "#{Post.count}."
+
+p = Post.last
+
+puts "#{Comment.count}."
+Comment.find_or_create_by!(post: p, body: "My comment")
+puts "#{Comment.count}."
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
