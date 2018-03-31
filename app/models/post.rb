d70d79f78d26d3d1ccc77b8870417770a6b8cf16
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-03-31 11:31:30 -0400

    Assignment 17 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  after_create :check_for_spam
+
+  def check_for_spam
+    if self.id % 5 == 0
+      self.update(title: "SPAM")
+    end
+  end
 end
