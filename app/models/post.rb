b36bd50fa9a4cf6aad292e0415e589bfd444043b
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-04-09 19:20:15 -0400

    Checkpoint-21 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :topic
   has_many :comments, dependent: :destroy
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+
   #after_create :check_for_spam
 
   #def check_for_spam
