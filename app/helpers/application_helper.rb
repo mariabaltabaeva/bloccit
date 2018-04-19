6df66c8a2c0619fb12bea516e61472a6e49e47ec
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-04-18 20:23:56 -0400

    Assignment-24:completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
 
   def avatar_url(user)
      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-     image_tag "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
    end
 end
