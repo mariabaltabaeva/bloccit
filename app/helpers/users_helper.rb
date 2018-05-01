68854e27bd6f2949aac0d32a15f7bba606afb152
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-04-30 20:08:08 -0400

    Assignment not completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def given_user(user)
+   if current_user != post.user || current_user != comment.user
+     flash[:notice] = "{user.name} has not submitted any posts yet."
+   end
+ end
 end
